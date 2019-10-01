88fb6bf883397a9b235a1820024835543bcbdccc
AdamLipiec <51492697+AdamLipiec@users.noreply.github.com>
2019-10-01 09:25:32 +0200

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,91 @@
+lock = false; up = 23; down = 0; gravity = true; x = 50, y = 0, jumpHandler = null, jumpCount = 0, frames = 20, deltaH = 2; dUp = up;
+
+document.onkeydown = jump;
+
+
+function ballStart()
+{
+	BallHandler = window.setInterval('move()', frames);
+}
+
+var keyState = {};
+window.addEventListener('keydown',function(e){
+    keyState[e.keyCode] = true;
+},true);
+
+window.addEventListener('keyup',function(e){
+    keyState[e.keyCode] = false;
+},true);
+
+function move() 
+{
+	if(x < 0 || y > 500)
+	{
+		window.clearInterval(MoveHandler);
+		window.clearInterval(BallHandler);
+		document.getElementById('btnStart').style.display = 'inline';
+		document.getElementById('btnStart').value = 'Restart';
+		restart = true;
+	}
+
+	if(y + 60 >= width - floor[x + 110])
+		x -= pace;
+
+	if(x <= 690 && (keyState[39] || keyState[68]) && y + 135 <= width - floor[x + 125])
+		x += 7;
+	if(x > 0 && (keyState[37] || keyState[65]) && y + 135 <= width - floor[x - 40])
+		x -= 7; 
+	if(y < width - floor[x] -135 && gravity)
+	{
+		y += down;
+		down += deltaH;
+
+		for(i = x; i < x + 90; i++)
+			if(y + down >= width - floor[i] - 135)
+			{
+				jumpCount = 0;
+				y = width - floor[i] - 135;
+				down = 0;
+			}
+	}
+	shiftBackBall = 36 - x;
+	shiftFrontBall = 800 - (x - 36);
+	document.getElementById('heros').style.clip = 'rect(0px,' + shiftFrontBall 
+	+ 'px, 150px,' + shiftBackBall+'px)';
+	document.getElementById('heros').style.left = x - 25 + 'px';
+	document.getElementById('heros').style.top = y + 'px';
+	//document.getElementById('tempBall').innerText='x: '+ x + ' y: ' + y;
+}
+
+function jump() 
+{
+	if (event.keyCode == 38 || event.keyCode == 87) 
+	{
+		if(jumpHandler != null)
+			window.clearInterval(jumpHandler);
+		gravity = false;
+	if(jumpCount < 2)
+	{
+		jumpCount++;
+		//document.getElementById('tempBall').innerText=jumpCount;
+		jumpHandler = window.setInterval('hop()', frames);
+	}
+	else 
+	 	gravity = true;
+	}
+}
+        
+
+function hop()
+{
+	gravity = false;
+	y -= dUp;
+	dUp -= deltaH;
+	if(dUp <= 0)
+	{
+		dUp = up;
+		window.clearInterval(jumpHandler);
+		jumpHandler = null;
+		gravity = true;
+	}
+} 
---
+++ Canvas.js
@@ -0,0 +1,154 @@
+heights = [300, 400];
+lenghts = [100, 200, 300];
+width = 800;
+height = 800;
+score = 0;
+pace = 10;
+countDist = 0;
+record = 0;
+
+
+Cloud = class {
+	constructor(xCloud)
+	{
+		this.xCloud = xCloud;
+	}
+    	cloudWidth = 0;
+	speed = 2;
+}
+
+CloudsParameters = [new Cloud(300), new Cloud(700)];
+
+Obstacle = class {
+  constructor(h, p0, length) {
+    this.h = h;
+    this.p0 = p0;
+    this.length = length;
+  }
+}
+
+function onLoad()
+{
+	canvas = document.getElementById('canvas1');
+	ctx = canvas.getContext('2d');
+	clouds = [document.getElementById('cloud_1').style,
+	document.getElementById('cloud_2').style];
+	
+	/*ctx.clearRect(0, 400, 800, 400);
+
+	p = [new Obstacle(300, 0, 800),
+	new Obstacle(heights[Math.floor(Math.random() * 2)], 
+	width + 400, dlugosc[Math.floor(Math.random() * 3)]),
+	new Obstacle(heights[Math.floor(Math.random() * 2)], 
+	width + 800, dlugosc[Math.floor(Math.random() * 3)])];*/
+
+	obstacles = [document.getElementById('carriage_1').style,
+	document.getElementById('carriage_2').style,
+	document.getElementById('carriage_3').style];
+
+}
+
+function generuj()
+{
+	MoveHandler = window.setInterval('backgroundMove()', 30);
+}
+
+function backgroundMove()
+{
+	/*for(j=0; j<floor.length;j++)
+		floor[j] = 100;
+	for(i = 0; i < p.length; i++)
+	{
+		ctx.clearRect(p[i].p0, height - p[i].h, p[i].length, p[i].h);
+		p[i].p0 -= pace;
+	}
+
+	for(i = 0; i < p.length; i++)
+		if(p[i].p0+p[i].length<=0)
+		{
+			p[i].p0 = width;
+			p[i].h = heights[Math.floor(Math.random() * 2)];	
+			p[i].length = lenghts[Math.floor(Math.random() * 3)];	
+		}
+
+	for(i = 0; i < p.length; i++)
+	{
+		ctx.fillRect(p[i].p0, height-p[i].h, p[i].length, p[i].h);
+		for(j = p[i].p0; j < p[i].p0 + p[i].length; j++)
+		{
+			if(floor[j] < p[i].h)
+				floor[j] = p[i].h;
+		}*/
+		score += 0.1;	
+		countDist ++;
+		if(score > record)
+			record = score;
+		if(countDist == 1000 && pace < 30)
+		{
+			countDist = 0;
+			pace += 2;
+		}		
+		//document.getElementById('temp').innerHTML = 'floor[x]: ' + floor[x + 150];
+		//document.getElementById('wynik').innerHTML = 'Distance: ' + Math.round(score);
+
+		//document.getElementById('wynik').innerHTML = 'Record: ' + Math.round(record);
+		ctx.font = "30px Arial";	
+		ctx.clearRect(570, 20, 250, 50);	
+		ctx.fillText("Distance: " + Math.round(score), 570, 50)
+
+		ctx.clearRect(0, 20, 250, 50);	
+		ctx.fillText("Record: " + Math.round(record), 30, 50)
+	//}
+	//WAGONY
+	for(j=0; j<floor.length;j++)
+		floor[j] = 100;
+
+	for(i = 0; i < obstacles.length; i++)
+	{
+		ObstacleParameters[i].p0 -= pace;
+		obstacles[i].left = ObstacleParameters[i].p0 + 25 + 'px';
+		obstacles[i].height = ObstacleParameters[i].h + 'px';
+		obstacles[i].width = ObstacleParameters[i].length + 'px';
+		obstacles[i].top = 810 - ObstacleParameters[i].h + 'px'; 
+		shiftBackObstacle = 788 - ObstacleParameters[i].p0;
+		shiftFrontObstacle = -15 - ObstacleParameters[i].p0;
+		obstacles[i].clip = 'rect(0px,' + shiftBackObstacle + 'px,' + ObstacleParameters[i].h 		+ 'px,'
+		+ shiftFrontObstacle + 'px)';
+		if(ObstacleParameters[i].p0 + ObstacleParameters[i].length < 0)
+		{
+			ObstacleParameters[i].p0 = width;
+			ObstacleParameters[i].h = heights[Math.floor(Math.random() * 2)];	
+			ObstacleParameters[i].length = lenghts[Math.floor(Math.random() * 3)];
+		}
+
+		for(j = ObstacleParameters[i].p0; j < ObstacleParameters[i].p0 + ObstacleParameters		[i].length; j++)
+			{
+				if(floor[j] < ObstacleParameters[i].h)
+					floor[j] = ObstacleParameters[i].h;
+			}
+	}
+
+
+
+	//CHMURY
+	for(i = 0; i < clouds.length; i++)
+	{
+		CloudsParameters[i].xCloud -= CloudsParameters[i].speed;
+		clouds[i].left = CloudsParameters[i].xCloud + 'px';
+		shiftBack = 812 - CloudsParameters[i].xCloud;
+		shiftFront = 10 - CloudsParameters[i].xCloud;
+		clouds[i].clip = 'rect(0px,' + shiftBack + 'px, 150px,'+ shiftFront + 'px)';
+		if(CloudsParameters[i].xCloud + 200 < 0)
+		{
+			CloudsParameters[i].speed = 1 + Math.random() * 6;
+			CloudsParameters[i].xCloud = 800 + Math.random() * 200;
+			CloudsParameters[i].cloudWidth = 100 + Math.random() * 100;
+		}
+	}
+}
+
+function canvasStart()
+{
+	onLoad();
+	generuj();
+}
